dc6b9e9844e7e29ddcf621e279574803178d8a0b
Ryo Yasuda <e165729@ie.u-ryukyu.ac.jp>
2016-12-05 17:39:12 +0900

    First commit

---
+++ src/main/jp/ac/uryukyu/ie/e165729/Main.java
@@ -0,0 +1,12 @@
+package jp.ac.uryukyu.ie.e165729;
+
+/**
+ * Created by e165729 on 2016/12/05.
+ */
+public class Main {
+    public static void main(String[] args){
+        String str = null;
+        System.out.println(str.length());
+    }
+
+}
